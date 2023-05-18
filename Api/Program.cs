145ab56f5a37b210528deaefcece15436732112f
nestor606 <maquilon.927@gmail.com>
2023-05-18 16:02:57 -0500

    se integra ajustes de cambios

--- Api/Program.cs
+++ Api/Program.cs
@@ -36,30 +36,6 @@ builder.Services.AddTransient<IServicioBase<ClsEmpleadoDom, int>, ClsEmpleadoSer
 
 #region "Authorizacion"
 
-var authenticationOptions = new KeycloakAuthenticationOptions
-{
-    AuthServerUrl = "http://localhost:8088/",
-    Realm = "Test",
-    Resource = "test-client",
-};
-
-builder.Services.AddKeycloakAuthentication(authenticationOptions);
-
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("users", policy =>
-               policy.RequireAssertion(context =>
-               context.User.HasClaim(c =>
-                       (c.Value == "user") || (c.Value == "admin"))));
-    //Create policy with only one claim
-    options.AddPolicy("admins", policy =>
-        policy.RequireClaim(ClaimTypes.Role, "admin"));
-    //Create a policy with a claim that doesn't exist or you are unauthorized to
-    options.AddPolicy("noaccess", policy =>
-        policy.RequireClaim(ClaimTypes.Role, "noaccess"));
-
-});
-
 #endregion
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
